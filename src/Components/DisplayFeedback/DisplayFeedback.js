c51e0f5924e59cca5376246bf477f1929e4dd428
raihan kawser <raihankawser29@gmail.com>
2021-11-19 23:50:15 +0600

    jifuhsduifg

--- src/Components/DisplayFeedback/DisplayFeedback.js
+++ src/Components/DisplayFeedback/DisplayFeedback.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 import useAuth from './../AuthProvider/UseAuth';
-
+import {MdStarRate } from "react-icons/md";
 const DisplayFeedback = ({feedback}) => {
     const {name,review,rating}=feedback;
     const{user}=useAuth(); 
@@ -11,8 +11,11 @@ const DisplayFeedback = ({feedback}) => {
       <Card>
         <Card.Img variant="top" src='https://www.marketing91.com/wp-content/uploads/2019/07/Importance-of-Corporate-Image-2.jpg'
          className='w-25 rounded-circle mx-auto' />
-        <Card.Body>
+        <Card.Body className='review-text'>
+          <div className='review-size'>
           <Card.Title>{name}</Card.Title>
+          <Card.Title>{rating}<MdStarRate className='text-danger'/> </Card.Title>
+          </div>       
           <Card.Text>
            {review}
           </Card.Text>
